6e4c561f9cf499a6f91c72f0dcc34c5b97ad9853
yangjinheng <42023641@qq.com>
2021-01-06 13:58:38 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+// Info 是每个子视频的元数据
+type Info struct {
+	Title     string `json:"Title"`
+	PartName  string `json:"PartName"`
+}
+
+func main() {
+	// 解析哔哩哔哩下载目录绝对路径
+	downloaddir, err := filepath.Abs(".")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// 如果 info 文件同目录有 flv 文件则整理
+	filepath.Walk(downloaddir, func(path string, info os.FileInfo, err error) error {
+		if filepath.Ext(path) == ".info" {
+			dir := filepath.Dir(path)
+			videosfile, ext := []string{}, ""
+			flvfile, err := filepath.Glob(filepath.Join(dir, "*.flv"))
+			if len(flvfile) !=0 && err == nil {
+				videosfile = flvfile
+				ext = ".flv"
+			}
+			mp4file, err := filepath.Glob(filepath.Join(dir, "*.mp4"))
+			if len(mp4file) !=0 && err == nil {
+				videosfile = mp4file
+				ext = ".mp4"
+			}
+			if len(videosfile) == 0 || err != nil || ext == "" {
+				return nil
+			}
+			infofile, err := ioutil.ReadFile(path)
+			if err != nil {
+				return nil
+			}
+			infostruct := Info{}
+			err = json.Unmarshal(infofile, &infostruct)
+			if err != nil {
+				return nil
+			}
+			if infostruct.PartName == "" {
+				infostruct.PartName = infostruct.Title
+			}
+			targetdir := filepath.Join(downloaddir, infostruct.Title)
+			_, err = os.Stat(targetdir)
+			if err != nil {
+				if os.IsNotExist(err) {
+					os.Mkdir(targetdir, os.ModeDir)
+				}
+			}
+			oldpath := videosfile[0]
+			newpath := filepath.Join(targetdir, infostruct.PartName + ext)
+			os.Rename(oldpath, newpath)
+		}
+		return nil
+	})
+}
